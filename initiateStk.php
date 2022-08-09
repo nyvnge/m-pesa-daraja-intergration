76b58cf1b8c26ff0017c37c730863a1bc651cc14
SandersonNyange <sandersonnyange@gmail.com>
2022-08-09 16:30:28 +0300

    Added Detailed Comments to Help with debbuging

--- initiateStk.php
+++ initiateStk.php
@@ -3,13 +3,17 @@ if(isset($_POST['submit'])){
 
 date_default_timezone_set('Africa/Nairobi');
 
+# Unique Access Points
 $consumerKey = "4vnt0YCtlrA7gd2xewLiWMqaCfUB5hyK";
 $consumerSecret = "5u25FKQMcwoY3VAv";
 
-
+# Definition of Variables
+# I Recommend you use the same variable names as used below. 
 $BusinessShortCode = "174379";
 $PassKey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
-
+# Party A is the phone number you want to initiate the mpesa stk. When Keying the phone number you are required to key it in the 254xxxxxxx format
+# AccountReference you can put any number.
+# TransactionDesc - Give the reason or describe the type of transaction you want to make
 $PartyA = $_POST['phoneNumber'];
 $AccountReference = "9238";
 $TransactionDesc = "Donation Funds";
@@ -19,14 +23,15 @@ $Timestamp = date("YmdHis");
 
 $Password = base64_encode($BusinessShortCode.$PassKey.$Timestamp);
 
- # header for access token
+ # Access token header
  $headers = ['Content-Type:application/json; charset=utf8'];
 
- # M-PESA endpoint urls
+ # URL M-PESA endpoints
 $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
 $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
-# callback url
+# Callback url
+# Once you have created an app with Heroku they will give you a unique URL which you will use to launch your app.
 $CallBackURL = 'https://hidden-plains-66369.herokuapp.com//callback_url.php';  
 
 $curl = curl_init($access_token_url);
@@ -40,16 +45,15 @@ $result = json_decode($result);
 $access_token = $result->access_token;  
 curl_close($curl);
 
-# header for stk push
+# Stk Push Header
 $stkheader = ['Content-Type:application/json','Authorization:Bearer '.$access_token];
 
-# initiating the transaction
+# Transaction initiation
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, $initiate_url);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
 
 $curl_post_data = array(
- //Fill in the request parameters with valid values
  'BusinessShortCode' => $BusinessShortCode,
  'Password' => $Password,
  'Timestamp' => $Timestamp,
